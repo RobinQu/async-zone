c3bc29fb84386f74c29c57aa2dbda77c8965b7b9
RobinQu <robinqu@gmail.com>
2016-09-01 20:13:47 +0800

    fix promise tests

--- test/promise_test.js
+++ test/promise_test.js
@@ -25,5 +25,47 @@ describe('Promise', function () {
     });
   });
 
+  it('should work with unwrapped promises', function (done) {
+    Zone.current.fork().run(function () {
+      Zone.current.set('abc', 123);
+      expect(Zone.current.get('abc')).to.equal(123);
+      Promise.resolve().then(function () {
+        expect(Zone.current.get('abc')).to.equal(123);
+        return Promise.resolve();
+      }).then(function () {
+        expect(Zone.current.get('abc')).to.equal(123);
+        return Promise.reject();
+      }).catch(function () {
+        expect(Zone.current.get('abc')).to.equal(123);
+        return Promise.resolve();
+      }).then(function () {
+        expect(Zone.current.get('abc')).to.equal(123);
+      }).then(done);
+    });
+  });
+
+  it('should work with forked continuations', function () {
+    return Zone.current.fork({
+      name: 'sunday'
+    }).run(function () {
+      const promise = Promise.resolve();
+      return Zone.current.fork({
+        name: 'saturday'
+      }).run(function () {
+        Zone.current.set('abc', 123);
+        return Promise.all([
+          promise.then(function () {
+            expect(Zone.current.get('abc')).to.equal(123);
+          }),
+          promise.then(function () {
+            expect(Zone.current.get('abc')).to.equal(123);
+          }),
+          promise.then(function () {
+            expect(Zone.current.get('abc')).to.equal(123);
+          })
+        ]);
+      });
+    });
+  });
 
 });
